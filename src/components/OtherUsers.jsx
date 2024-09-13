d924fa426ba756ee7231425056d1095c881b4fc5
sweat kamal <sweatkamal152@gmail.com>
2024-09-13 14:06:40 +0530

    d

--- src/components/OtherUsers.jsx
+++ src/components/OtherUsers.jsx
@@ -34,12 +34,10 @@ import useGetOtherUsers from '../hooks/userGetOtherUsers';
 import { useSelector } from 'react-redux';
 
 const OtherUsers = () => {
-    // Custom hook to fetch other users
     useGetOtherUsers();
     const { otherUsers } = useSelector(store => store.user);
 
-    // Ensure otherUsers is an array before using map
-    if (!Array.isArray(otherUsers)) return null; // Early return with null to avoid rendering issues
+    if (!Array.isArray(otherUsers)) return null; 
 
     return (
         <div className='overflow-auto flex-1'>
