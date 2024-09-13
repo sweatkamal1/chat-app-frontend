424181d06e72f6861d0b66c1f08c3a8a873ad803
sweat kamal <sweatkamal152@gmail.com>
2024-09-13 11:58:21 +0530

    d

--- src/components/OtherUsers.jsx
+++ src/components/OtherUsers.jsx
@@ -1,27 +1,53 @@
-import React from 'react'
+// import React from 'react'
+// import OtherUser from './OtherUser';
+// import useGetOtherUsers from '../hooks/userGetOtherUsers';
+// import {useSelector} from "react-redux";
+
+
+// const OtherUsers = () => {
+//     // my custom hook
+//     useGetOtherUsers();
+//     const {otherUsers} = useSelector(store=>store.user);
+//     if (!otherUsers) return; // early return in react
+     
+//     return (
+//         <div className='overflow-auto flex-1'>
+//             {
+//                 otherUsers?.map((user)=>{
+//                     return (
+//                         <OtherUser key={user._id} user={user}/>
+//                     )
+//                 })
+//             }
+            
+//         </div>
+//     )
+// }
+
+// export default OtherUsers
+
+
+
+import React from 'react';
 import OtherUser from './OtherUser';
 import useGetOtherUsers from '../hooks/userGetOtherUsers';
-import {useSelector} from "react-redux";
-
+import { useSelector } from 'react-redux';
 
 const OtherUsers = () => {
-    // my custom hook
+    // Custom hook to fetch other users
     useGetOtherUsers();
-    const {otherUsers} = useSelector(store=>store.user);
-    if (!otherUsers) return; // early return in react
-     
+    const { otherUsers } = useSelector(store => store.user);
+
+    // Ensure otherUsers is an array before using map
+    if (!Array.isArray(otherUsers)) return null; // Early return with null to avoid rendering issues
+
     return (
         <div className='overflow-auto flex-1'>
-            {
-                otherUsers?.map((user)=>{
-                    return (
-                        <OtherUser key={user._id} user={user}/>
-                    )
-                })
-            }
-            
+            {otherUsers.map(user => (
+                <OtherUser key={user._id} user={user} />
+            ))}
         </div>
-    )
-}
+    );
+};
 
-export default OtherUsers
+export default OtherUsers;
