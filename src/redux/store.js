edba27df95f3bdbff1ac8c3174557b0d57d35bc4
sweat kamal <sweatkamal152@gmail.com>
2024-09-27 11:09:59 +0530

    s

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,30 +11,32 @@ import {
     PURGE,
     REGISTER,
   } from 'redux-persist';
-  import storage from 'redux-persist/lib/storage'
+import storage from 'redux-persist/lib/storage'
 
-  const persistConfig = {
+const persistConfig = {
     key: 'root',
     version: 1,
     storage,
-  }
+    blacklist: ['socket'] // socket state को persist नहीं करेंगे
+}
 
-  const rootReducer = combineReducers({
-    user:userReducer,
-    message:messageReducer,
-    socket:socketReducer
- })
+const rootReducer = combineReducers({
+    user: userReducer,
+    message: messageReducer,
+    socket: socketReducer
+})
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-
 const store = configureStore({
-    reducer:persistedReducer,
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredPaths: ['socket'], // यहां भी socket को ignore करेंगे
       },
     }),
 });
+
 export default store;
