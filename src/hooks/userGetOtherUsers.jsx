f9de5ed2b94926a923dcd4873538ef5611ed9f79
sweat kamal <sweatkamal152@gmail.com>
2024-09-27 12:16:47 +0530

    s

--- src/hooks/userGetOtherUsers.jsx
+++ src/hooks/userGetOtherUsers.jsx
@@ -1,3 +1,41 @@
+// import React, { useEffect } from 'react';
+// import axios from "axios";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setOtherUsers } from '../redux/userSlice';
+
+// const useGetOtherUsers = () => {
+//     const dispatch = useDispatch();
+//     const { authUser } = useSelector(store => store.user);  // Assuming you store user info including token
+
+//     useEffect(() => {
+//         const fetchOtherUsers = async () => {
+//             try {
+//                 axios.defaults.withCredentials = true;
+                
+//                 // Add token to Authorization header if it exists
+//                 const config = {
+//                     headers: {
+//                         Authorization: `Bearer ${authUser?.token}`,  // assuming token is part of authUser
+//                     },
+//                 };
+                
+//                 const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/v1/user`, config);
+//                 console.log("other users -> ", res);
+//                 dispatch(setOtherUsers(res.data));
+//             } catch (error) {
+//                 console.log("Error fetching users:", error);
+//             }
+//         }
+//         if (authUser) {
+//             fetchOtherUsers();
+//         }
+//     }, [authUser, dispatch]);
+
+// }
+
+// export default useGetOtherUsers;
+
+
 import React, { useEffect } from 'react';
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
@@ -5,32 +43,30 @@ import { setOtherUsers } from '../redux/userSlice';
 
 const useGetOtherUsers = () => {
     const dispatch = useDispatch();
-    const { authUser } = useSelector(store => store.user);  // Assuming you store user info including token
+    const { authUser } = useSelector(store => store.user);
 
     useEffect(() => {
         const fetchOtherUsers = async () => {
+            if (!authUser) return; // Check if authUser is available
+
             try {
-                axios.defaults.withCredentials = true;
-                
-                // Add token to Authorization header if it exists
                 const config = {
                     headers: {
-                        Authorization: `Bearer ${authUser?.token}`,  // assuming token is part of authUser
+                        Authorization: `Bearer ${authUser.token}`, // assuming token is part of authUser
                     },
+                    withCredentials: true, // set this here for each request
                 };
-                
+
                 const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/v1/user`, config);
-                console.log("other users -> ", res);
+                console.log("Other users -> ", res.data);
                 dispatch(setOtherUsers(res.data));
             } catch (error) {
-                console.log("Error fetching users:", error);
+                console.error("Error fetching users:", error.response?.data || error.message);
             }
-        }
-        if (authUser) {
-            fetchOtherUsers();
-        }
-    }, [authUser, dispatch]);
+        };
 
+        fetchOtherUsers();
+    }, [authUser, dispatch]);
 }
 
 export default useGetOtherUsers;
